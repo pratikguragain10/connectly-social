3ef591aac9426cff60ae6550d9e6d50102a78560
pratikguragain10 <pratikguragain69@gmail.com>
2026-01-02 11:05:40 +0530

    Connectly

--- facebook/facebook/settings.py
+++ facebook/facebook/settings.py
@@ -13,6 +13,9 @@ ALLOWED_HOSTS = os.environ.get(
     "localhost,127.0.0.1,connectly-social-t3yx.onrender.com"
 ).split(",")
 
+# --------------------------------------------------
+# APPLICATIONS
+# --------------------------------------------------
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -20,58 +23,90 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Cloudinary
     'cloudinary',
     'cloudinary_storage',
-    
-    # REQUIRED for allauth
+
+    # Sites (required by allauth)
     'django.contrib.sites',
 
-    # allauth core
+    # Allauth
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
 
-    # provider (Google)
+    # Google provider
     'allauth.socialaccount.providers.google',
 
+    # Local apps
     'user',
 ]
 
 SITE_ID = 1
 
+# --------------------------------------------------
+# MIDDLEWARE
+# --------------------------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
-    # Static files (Render / production)
+    # Static files (Render)
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
 
-    # Authentication
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 
-    # REQUIRED for django-allauth
+    # Required by allauth
     'allauth.account.middleware.AccountMiddleware',
 
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# --------------------------------------------------
+# AUTHENTICATION
+# --------------------------------------------------
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+# Allauth configuration
+ACCOUNT_LOGIN_METHODS = {'email'}
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'password1*', 'password2*']
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+SOCIALACCOUNT_LOGIN_ON_GET = True
+
+# --------------------------------------------------
+# SECURITY (Render / HTTPS)
+# --------------------------------------------------
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://connectly-social-t3yx.onrender.com",
+]
+
+# --------------------------------------------------
+# URLS / WSGI
+# --------------------------------------------------
 ROOT_URLCONF = 'facebook.urls'
 WSGI_APPLICATION = 'facebook.wsgi.application'
 
+# --------------------------------------------------
+# TEMPLATES
+# --------------------------------------------------
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -79,7 +114,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request',  # REQUIRED
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -87,6 +122,9 @@ TEMPLATES = [
     },
 ]
 
+# --------------------------------------------------
+# DATABASE
+# --------------------------------------------------
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
@@ -95,13 +133,16 @@ DATABASES = {
     )
 }
 
+# --------------------------------------------------
+# STATIC FILES
+# --------------------------------------------------
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# -----------------------------
+# --------------------------------------------------
 # CLOUDINARY (MEDIA)
-# -----------------------------
+# --------------------------------------------------
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
@@ -111,11 +152,12 @@ CLOUDINARY_STORAGE = {
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/'
 
+# --------------------------------------------------
+# EMAIL
+# --------------------------------------------------
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# --------------------------------------------------
+# DEFAULT PK
+# --------------------------------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://connectly-social-t3yx.onrender.com",
-]
-
